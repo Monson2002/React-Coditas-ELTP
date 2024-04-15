9ea0e340746a15cbd27f1c1bfd89dc16be609022
Monson <monsonreji.verghese@coditas.com>
2024-04-15 14:03:08 +0530

    Added 15-4-24 using SASS : 2:03pm 15-4-24

---
+++ 15-4-24/src/App.tsx
@@ -0,0 +1,17 @@
+import Server from './components/Server';
+import styles from './App.module.scss';
+
+function App() {
+
+  return (
+    <>
+      <div className={styles.App}>
+        <Server name="EC2-M2" isActive={true}/> 
+        <Server name="EC2-L4" isActive={false}/>  
+        <Server name="EC2-C4" isActive={true}/>     
+      </div>   
+    </>
+  )
+}
+
+export default App
---
+++ 15-4-24/src/components/Server.tsx
@@ -0,0 +1,40 @@
+import styles from './Server.module.scss';
+
+type ServerType = {
+  name: string,
+  isActive: boolean
+}
+
+const Server = (props: ServerType) => {
+
+  return (
+    <>
+      {/* <div className={styles.server}>
+        {props.name}
+        {
+          props.status === "On"
+          ? <div className={styles.statusOn}>Online</div>
+          : <div className={styles.statusOff}>Offline</div>
+        }
+        {
+          props.status === "On" 
+          ? <button className={styles.btn}>Stop Server</button>
+          : <button className={styles.btn}>Start Server</button>
+        }
+      </div> */}
+      <div className={styles.Server}>
+        <h2 className={styles.Header}>{props.name}</h2>
+        <div>
+          <div className={`${props.isActive ? styles.PillOn : styles.PillOff}`}>
+            {props.isActive ? "Online" : "Offline"}
+          </div>
+          <button className={styles.Btn}>
+            {props.isActive ? "Stop" : "Start"}
+          </button>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Server;
