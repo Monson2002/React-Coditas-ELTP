dea1153ce2a7f13c6cbacc2ec86f681c6d4edb9f
Monson <monsonreji.verghese@coditas.com>
2024-04-16 11:35:21 +0530

    Added 16-4-24 and SCSS-SASS

---
+++ 16-4-24/src/App.tsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import styles from './App.module.scss'
+
+
+function App() {
+  return (
+    <>
+      <div className={`${styles.centered} ${styles.container}`}>
+        <div>
+          <h1>Hello, World</h1>
+        </div>
+        <div className={`${styles.items}`}>
+          <span className={`${styles.SpanElement}`}>
+            Some Text
+          </span>
+        </div>
+        <div>
+          <button className={`${styles.primary}`}>Button1</button>
+          <button className={`${styles.secondary}`}>Button2</button>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
