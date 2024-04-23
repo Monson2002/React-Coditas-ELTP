71f07104b1f9f8124d014f504a148ad0b7bfbcea
Monson <monsonreji.verghese@coditas.com>
2024-04-23 17:18:42 +0530

    Changed 23-4-24 to use a single 'increment' function

--- 23-4-24/src/App.tsx
+++ 23-4-24/src/App.tsx
@@ -7,18 +7,18 @@ function App() {
   let [count1, setCount1] = useState(0);
   let [count2, setCount2] = useState(0);
 
-  const increment1 = () => {
-    setCount1(++count1);
-  }
-
-  const increment2 = () => {
-    setCount2(++count2);
+  const increment = (num: number) => {
+    if (num === 1) {
+      setCount2(++count2);
+    } else {
+      setCount1(++count1)
+    }
   }
 
   return (
     <div className={styles.App}>
-      <Counter count={count1} increment={() => increment2()}/>
-      <Counter count={count2} increment={() => increment1()}/>
+      <Counter count={count1} increment={() => increment(1)}/>
+      <Counter count={count2} increment={() => increment(2)}/>
     </div>
   )
 }
