61134c8fc24f684e598839f4594b5fb4fddaf451
Monson <monsonreji.verghese@coditas.com>
2024-05-09 10:39:44 +0530

    TS: 10:39am

--- PokemonApp/src/components/Main/Main.tsx
+++ PokemonApp/src/components/Main/Main.tsx
@@ -12,43 +12,42 @@ const Main = ({ }: MainProps) => {
 
   const [pokemons, setPokemons] = useState<PokemonProps[]>([]);
 
-  const LeftArrow = (idObj: {id: number}) => <ArrowCircleLeftSharpIcon onClick={() => handleToggleCaught(idObj)}/>
-  const RightArrow = (idObj: {id: number}) => <ArrowCircleRightSharpIcon onClick={() => handleToggleCaught(idObj)}/>
+  const LeftArrow = (idObj: { id: number }) => <ArrowCircleLeftSharpIcon onClick={() => handleToggleCaught(idObj)} />
+  const RightArrow = (idObj: { id: number }) => <ArrowCircleRightSharpIcon onClick={() => handleToggleCaught(idObj)} />
 
   const allPokemon = pokemons.filter((pokemon) => !pokemon.caught);
   const caughtPokemon = pokemons.filter((pokemon) => pokemon.caught);
 
   const getPokemonFromAPI = async () => {
     const dataFromAPI = await getPokemonData();
-    setPokemons(dataFromAPI);  
-    
+    setPokemons(dataFromAPI);
   }
 
-  const handleToggleCaught = (idObject: {id: number}) => {
+  const handleToggleCaught = (idObject: { id: number }) => {
     const selectedPokemon = pokemons.find((pokemon) => pokemon.id === idObject.id)!;
     selectedPokemon!.caught = !selectedPokemon.caught;
     setPokemons([...pokemons]);
   }
 
-useEffect(() => {
-  getPokemonFromAPI();
-}, [])
+  useEffect(() => {
+    getPokemonFromAPI();
+  }, [])
 
-return (
-  <div className={styles.Main}>
-    <PokemonList 
-    title="All Pokemon"
-    data={allPokemon}
-    rightArrow={RightArrow}
-    onClick={handleToggleCaught}
-    />
-    <PokemonList 
-    title="Caught Pokemon"
-    data={caughtPokemon}
-    leftArrow={LeftArrow}
-    onClick={handleToggleCaught}/>
-  </div>
-); 
+  return (
+    <div className={styles.Main}>
+      <PokemonList
+        title="All Pokemon"
+        data={allPokemon}
+        rightArrow={RightArrow}
+        onClick={handleToggleCaught}
+      />
+      <PokemonList
+        title="Caught Pokemon"
+        data={caughtPokemon}
+        leftArrow={LeftArrow}
+        onClick={handleToggleCaught} />
+    </div>
+  );
 };
 
 export default Main;  
